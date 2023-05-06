b926ec61e2cd487b76881f778f14d1684cc9bd66
Albert Andersen <albert3257@live.dk>
2023-05-07 01:22:57 +0200

    work on user page and calender page

---
+++ Shared/Requests/CreateEventRequest.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Dto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Requests
+{
+    public class CreateEventRequest
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public EventType EventType { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        // relationships
+        public GroupDto Group { get; set; } = new GroupDto();
+    }
+}
--- Utensils-Api/Controllers/EventController.cs
+++ Utensils-Api/Controllers/EventController.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Shared.Dto.Models;
+using Shared.Requests;
+using Utensils_Api.Database;
+using Utensils_Api.Database.Models;
 
 namespace Utensils_Api.Controllers
 {
     [Route("api/events")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
+    [AllowAnonymous]
     public class EventController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<int> Get()
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
+
+        public EventController(
+            ApplicationDbContext context,
+            IMapper mapper
+        ) {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost("create")]
+        public ActionResult<EventDto> CreateEvent([FromBody] CreateEventRequest request)
         {
-            return 5;
+            Event eventToSave = new Event()
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Description = request.Description,
+                EventType = request.EventType,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                Group = _mapper.Map<Group>(request.Group)
+            };
+
+            _context.Events.Add(eventToSave);
+            _context.SaveChanges();
+
+            return Ok(_mapper.Map<EventDto>(eventToSave));
         }
     }
 }
